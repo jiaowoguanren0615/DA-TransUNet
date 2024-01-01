6e295b5420636ad14f5fdecbbf7870945f67ee62
huangshuqi <1105374939@qq.com>
2024-01-01 22:27:08 +0800

    update the Dataset class of VOC & COCO & CityScapes

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,1 +1,7 @@
 from .synapse import Synapse_dataset
+from .cityscapes import CityscapesSegmentation
+from .coco import COCOSegmentation
+from .voc import VOCSegmentation
+from .mypath import Path
+from .utils import *
+from .custom_transforms import *
